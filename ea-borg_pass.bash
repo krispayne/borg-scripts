9be13819240bc533cdcf615562ed75bf8ca0cdb4
Kris Payne <krispayne@users.noreply.github.com>
2018-01-04 11:33:53 -0800

    adding extension attribute

---
+++ ea-borg_pass.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+# grabs the content of a file and prints it to the result var
+
+borg_pass_file=/var/hidden/dir//borg.conf
+
+if [ -e ${borg_pass_file} ];then
+  borg_pass=$(cat ${borg_pass_file})
+  echo "<result>${borg_pass}</result>"
+else
+  echo "<result>No borg_pass_file</result>"
+fi
